025cd7a1875a48f2e391f7aa39bf2841c61bd157
rurua-dayo <pandemic.hearts@icloud.com>
2026-07-01 15:51:17 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ window.OSHI_CONFIG = {
   xUrl: "https://x.com/mimori_ch?s=20",
   marshmallowUrl: "https://marshmallow-qa.com/neea1887n22ec1l?t=weSsQa&utm_medium=url_text&utm_source=promotion",
   litlinkUrl: "https://lit.link/mimoringo",
-
+　membershipUrl: "https://www.youtube.com/@mimori_ch/join",
+  
   appsScriptUrl: "https://script.google.com/macros/s/AKfycbyKGrZQDjYneH5Q9KIFzni1Uh9dGzbIPSwD3UJobzIgO1f8FRJht2qmjEk-HenYbvdj/exec"
 };
-membershipUrl: "https://www.youtube.com/@mimori_ch/join",
+
